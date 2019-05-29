14d92d66193ab98045fe74e853aad67f70bf2397
Amir Hossein Zamani <ahz.975@yahoo.com>
2019-05-29 09:54:54 -0400

    Q4 is solved.

--- Q4/main.py
+++ Q4/main.py
@@ -1,4 +1,5 @@
 from random import uniform
+from statistics import mean
 
 xLists = []
 yLists = []
@@ -6,22 +7,39 @@ yLists = []
 def isInCircle(x, y):
 	if(x**2 + y**2 <= 0.25):
 		return 1
-
 	else:
 		return 0
 
 def generateRandomPoints(numberOfRandomPoints):	
+	xLists.clear()
+	yLists.clear()
 	for i in range(numberOfRandomPoints):
 		xLists.append(uniform(-0.5, 0.5))
 	for i in range(numberOfRandomPoints):
 		yLists.append(uniform(-0.5, 0.5))
 
-def caculatePI():
+def caculatePI(numberOfRandomPoints):
 	pointsInCircleCount = 0
-	for (index, value) in enumerate(range(len(xLists))):		
+	generateRandomPoints(numberOfRandomPoints)
+	for (index, value) in enumerate(range(numberOfRandomPoints)):		
 		if isInCircle(xLists[index], yLists[index]):
 			pointsInCircleCount += 1
-	return pointsInCircleCount/(len(xLists)-pointsInCircleCount)
+	return 4*pointsInCircleCount/numberOfRandomPoints
 
-generateRandomPoints(10000)
-print(caculatePI())
+def find():
+	desiredCounter = 1
+	calculatedPI = 0.0
+	while abs(calculatedPI - 3.1) > 0.1:
+		generateRandomPoints(desiredCounter)
+		calculatedPI = caculatePI(desiredCounter)		
+		desiredCounter += 1	
+	return desiredCounter		
+
+repeatNumber = int(input("Please enter the number of repeat:"))
+counter = 0
+caculatePIs = []
+
+while counter < repeatNumber:
+	caculatePIs.append(caculatePI(find()))
+	counter += 1
+print(mean(caculatePIs))
