2e6565068675140195b8e181652d97754c35ba8a
Amir Hossein Zamani <ahz.975@yahoo.com>
2019-05-29 11:04:21 -0400

    Create a file in your own directory.

--- Q5/main.py
+++ Q5/main.py
@@ -6,5 +6,12 @@ def create_dir(name, address):
 		os.makedirs(directory)
 
 
+def create_file(name, address):
+	directory = str(address)
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	with open(os.path.join(directory, str(name)), 'w'):
+		pass
 
 create_dir('ahz', 'ahz')
+create_file('ahz.txt', 'ahz')
