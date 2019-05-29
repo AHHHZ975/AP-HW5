6025d32cbc8a3c850ad9a96b50af1bdffdcb326f
Amir Hossein Zamani <ahz.975@yahoo.com>
2019-05-29 11:20:22 -0400

    Find all of a specificfile directories && Q5 is solved.

--- Q5/main.py
+++ Q5/main.py
@@ -18,7 +18,13 @@ def delete(name, address):
 	if os.path.isfile(directory):
 		os.remove(directory)
 
+def find(name, address):
+	for root, dirs, files in os.walk(str(address)):
+		for file in files:
+			if file.endswith(str(name)):
+				print(os.path.join(root, file))	
 
 create_dir('ahz', 'ahz')
 create_file('ahz.txt', 'ahz')
 delete('ahz.txt', 'ahz')
+find('ahz.txt', 'ahz')
