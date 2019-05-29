1c45dd315b1a6e21d260e31e82ea7bc7d20aa343
Amir Hossein Zamani <ahz.975@yahoo.com>
2019-05-29 11:10:04 -0400

    Delete a file from a directory.

--- Q5/main.py
+++ Q5/main.py
@@ -13,5 +13,12 @@ def create_file(name, address):
 	with open(os.path.join(directory, str(name)), 'w'):
 		pass
 
+def delete(name, address):
+	directory = str(address) + '/' + str(name)
+	if os.path.isfile(directory):
+		os.remove(directory)
+
+
 create_dir('ahz', 'ahz')
 create_file('ahz.txt', 'ahz')
+delete('ahz.txt', 'ahz')
